a3d6d9edacdee16c291bb401cacf88f5ce1df980
arsenygolubtsov <32494075+arsenygolubtsov@users.noreply.github.com>
2018-04-19 11:34:29 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout); //выводит текст в стандартный поток
+void print(const std::string& text, std::ofstream& out); //выводит текст в файл
